a198db8287e9488da061074bd3094b462e4dca12
haitao <1458539260@qq.com>
2023-06-27 09:42:08 +0800

    day1study

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+    <input v-model="inputValue" type="text">
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, watch, watchEffect } from 'vue';
+
+const inputValue = ref('');
+const message = ref('');
+
+// 使用 watchEffect 创建副作用函数
+watchEffect(() => {
+  message.value = `你输入了：${inputValue.value}`;
+  console.log('watchEffect', inputValue.value);
+});
+
+// 使用 watch 创建侦听器
+watch(inputValue, (newValue, oldValue) => {
+  console.log('watch', newValue, oldValue);
+});
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1 @click="$emit('nihao','你好世界')">子组件数据:</h1>
+        <button @click="fn">点击传参</button>
+    </div>
+</template>
+<script setup lang="ts">
+const nihao = defineEmits(['nihao'])
+function fn(){
+    nihao('nihao','你好世界') 
+}
+</script>
